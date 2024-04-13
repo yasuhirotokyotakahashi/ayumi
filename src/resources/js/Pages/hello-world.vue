a5a7117c8165a6f576beff9daae35b5bd7c9b7f2
yasuhiro takahashi <suya.ty0911@gmail.com>
2024-04-13 18:20:17 +0900

    Inertiainstall

---
+++ src/resources/js/Pages/hello-world.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            greeting: "Hello World2!",
+        };
+    },
+};
+</script>
+
+<template>
+    <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style>
+.greeting {
+    color: red;
+    font-weight: bold;
+}
+</style>
