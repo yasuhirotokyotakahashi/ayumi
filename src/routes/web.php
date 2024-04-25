24409ad7f99a6f33ff38278c70a5f834fac0e68a
yasuhiro takahashi <suya.ty0911@gmail.com>
2024-04-25 13:30:54 +0900

    20240425-5

--- src/routes/web.php
+++ src/routes/web.php
@@ -42,7 +42,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // 上記はデフォルト
+    // 上記はデフォルトで入っていました。
 
     Route::post('/item/like/{item:id}', [LikeController::class, 'create'])->name('like.create');
     Route::post('/item/unlike/{item:id}', [LikeController::class, 'destroy'])->name('like.destroy');
